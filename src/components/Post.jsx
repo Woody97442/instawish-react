e8fb118c2e869276eebd572e5c34c546ba2bae80
Woody97442 <woody97442@hotmail.fr>
2024-01-09 09:12:32 +0400

    mise en forme de la page home

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import { IoEllipsisVerticalOutline } from "react-icons/io5";
+import { FiMessageCircle } from "react-icons/fi";
+import { MdFavoriteBorder } from "react-icons/md";
+import { BsSend } from "react-icons/bs";
+import { CiBookmark } from "react-icons/ci";
 
 function Post() {
   return (
@@ -23,6 +27,20 @@ function Post() {
           alt="paysage"
           className="post-img"
         />
+      </div>
+      <div className="flex flex-row w-full items-center justify-between mt-5">
+        <div className="flex flex-row items-center icon-left-block">
+          <FiMessageCircle className="icon-post" />
+          <div className="like-icon">
+            <MdFavoriteBorder className="icon-post" />
+          </div>
+          <BsSend className="icon-post" />
+        </div>
+        <CiBookmark className="icon-post" />
+      </div>
+      <div className="flex flex-row w-full items-center justify-between mt-3">
+        <p>31 Likes</p>
+        <p>15 minutes ago</p>
       </div>
     </div>
   );
