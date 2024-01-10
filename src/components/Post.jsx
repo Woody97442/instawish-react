f14a6dde2a1f53b077996371d57a0158b7231c02
Woody97442 <woody97442@hotmail.fr>
2024-01-10 15:48:31 +0400

    Ajoute et suppression des commentaires TODO protéger la suppression pour les commentaires de l'utilisateur. Affichage des commentaires en dessous des publications.

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -4,9 +4,11 @@ import { FiMessageCircle } from "react-icons/fi";
 import { MdFavoriteBorder } from "react-icons/md";
 import { BsSend } from "react-icons/bs";
 import { CiBookmark } from "react-icons/ci";
+import { IoIosCloseCircleOutline } from "react-icons/io";
+
 import { useDispatch, useSelector } from "react-redux";
 import { fetchMyFollows } from "../features/userSlice";
-
+import { fetchUser } from "../features/userSlice";
 function Post({ post }) {
   const dispatch = useDispatch();
   const { id, imageUrl, likeds, comments, createdBy, description } = post;
@@ -15,9 +17,61 @@ function Post({ post }) {
   const [liked, setLiked] = useState(
     likeds.some((like) => like.user.id === user.id)
   );
+  const [commentText, setCommentText] = useState("");
 
-  const handleComment = () => {
-    console.log("Comment");
+  const handleComment = async () => {
+    if (commentText.length > 3) {
+      let headersList = {
+        "Accept": "*/*",
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + localStorage.getItem("token"),
+      };
+
+      let bodyContent = {
+        "content": commentText,
+      };
+
+      let response = await fetch(
+        `https://symfony-instawish.formaterz.fr/api/comment/add/${id}`,
+        {
+          method: "POST",
+          body: JSON.stringify(bodyContent),
+          headers: headersList,
+        }
+      );
+      dispatch(fetchMyFollows());
+      dispatch(fetchUser());
+      setCommentText("");
+    }
+  };
+
+  const handleDelete = async (comment) => {
+    console.log(comment.id);
+    let headersList = {
+      "Accept": "*/*",
+      "Authorization": "Bearer " + localStorage.getItem("token"),
+    };
+
+    let response = await fetch(
+      `https://symfony-instawish.formaterz.fr/api/comment/remove/${comment.id}`,
+      {
+        method: "POST",
+        headers: headersList,
+      }
+    );
+    let data = await response.json();
+    dispatch(fetchMyFollows());
+    dispatch(fetchUser());
+  };
+
+  const handleCommentText = (e) => {
+    const sendIcon = document.getElementById("sendComment" + id);
+    if (commentText === "") {
+      sendIcon.classList.remove("message-send");
+    } else {
+      sendIcon.classList.add("message-send");
+    }
+    setCommentText(e.target.value);
   };
 
   const handleLike = async () => {
@@ -51,7 +105,7 @@ function Post({ post }) {
             <span>{createdBy.username}</span>
           </div>
         </div>
-        <IoEllipsisVerticalOutline className="circle-dot" />
+        <IoEllipsisVerticalOutline className="circle-dot cursor-pointer hover:scale-105 ease-in-out duration-300" />
       </div>
       <div className="post">
         <img
@@ -61,10 +115,39 @@ function Post({ post }) {
         />
       </div>
       <div className="flex flex-row w-full items-center justify-between mt-3 desc-post">
-        <span>{description}</span>
-        <span className="enable-comment">
-          Voir les {comments.length} commentaires
-        </span>
+        <div className="flex flex-row w-full items-center justify-between">
+          <div className="flex flex-col">
+            <span>{description}</span>
+            <span className="enable-comment">
+              Voir les {comments.length} commentaires
+            </span>
+            {comments.map((comment) => (
+              <div className="flex flex-row w-full items-center relative m-3 ">
+                <img
+                  src={baseUrlImage + comment.user.imageUrl}
+                  alt="Logo"
+                  className="w-12 h-12 rounded-full user-avatar"
+                />
+                <div className="user-post-author">
+                  <span>{comment.content}</span>
+                </div>
+                <IoIosCloseCircleOutline
+                  className=" ms-3 circle-dot cursor-pointer hover:scale-105 ease-in-out duration-300"
+                  onClick={(e) => handleDelete(comment)}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="flex flex-row w-full items-center justify-between">
+          <input
+            type="text"
+            placeholder="commenter..."
+            className="w-full h-10 rounded-3xl border border-gray-300 outline-none px-4 mt-2"
+            id="addComment"
+            onInput={handleCommentText}
+          />
+        </div>
       </div>
       <div className="flex flex-row w-full items-center justify-between mt-5">
         <div className="flex flex-row items-center icon-left-block">
@@ -82,7 +165,11 @@ function Post({ post }) {
             )}
             <p className="like-count">{likeds.length}</p>
           </div>
-          <BsSend className="icon-post" />
+          <BsSend
+            id={"sendComment" + id}
+            className="icon-post cursor-pointer hover:scale-105 ease-in-out duration-300 sendComment"
+            onClick={handleComment}
+          />
         </div>
         <CiBookmark className="icon-post" />
       </div>
